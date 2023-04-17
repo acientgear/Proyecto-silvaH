10327ec2fe3a5398f23166f7a6a3a5cd19f2b7b5
IgnacioGrez <ignacio.grez.m@usach.cl>
2023-04-17 19:02:31 -0400

    [Ignacio Grez]-Entidad Factura

---
+++ silvahnos-be/src/main/java/com/app/silvahnosbe/entities/FacturaEntity.java
@@ -0,0 +1,30 @@
+package com.app.silvahnosbe.entities;
+
+import java.sql.Timestamp;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "factura")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class IngresoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+    private Timestamp fecha_emision;
+    private Timestamp fecha_vencimiento;
+    private Timestamp fecha_pago;
+    private String observaciones;
+    private boolean borrado;
+    private Timestamp fecha_creacion;
+    private Timestamp fecha_modificacion;
+    private Timestamp fecha_borrado;
+    private int numero_factiura;
+}
