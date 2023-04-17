3574d933afd24cb3c68d4ce8731d65e4ef9b75a0
LeoVergaraS <leo.vergara@usach.cl>
2023-04-17 19:30:16 -0400

    [Leo Vergara]-Controlador Ingreso

---
+++ silvahnos-be/src/main/java/com/app/silvahnosbe/controllers/IngresoController.java
@@ -0,0 +1,46 @@
+package com.app.silvahnosbe.controllers;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.silvahnosbe.entities.IngresoEntity;
+import com.app.silvahnosbe.services.IngresoService;
+
+@RestController
+@RequestMapping("/ingresos")
+public class IngresoController {
+    @Autowired
+    IngresoService ingresoService;
+
+    @GetMapping
+    public ResponseEntity<ArrayList<IngresoEntity>> getAllIngresos(){
+        ArrayList<IngresoEntity> ingresos= ingresoService.obtenerIngresos();
+        if(ingresos == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(ingresos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<IngresoEntity> getIngresoById(@PathVariable("id") Long id){
+        IngresoEntity ingreso = ingresoService.obtenerIngresoPorId(id);
+        if(ingreso == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(ingreso);
+    }
+
+    @PostMapping
+    public ResponseEntity<IngresoEntity> createIngreso(@RequestBody IngresoEntity ingreso){
+        IngresoEntity ingresoGuardado = ingresoService.guardarIngreso(ingreso);
+        return ResponseEntity.ok().body(ingresoGuardado);
+    }
+}
