16419bb97a02a339799d783de69e2b3ceeda1297
LeoVergaraS <leo.vergara@usach.cl>
2023-04-17 19:06:11 -0400

    [Leo Vergara]-Servicio Ingreso

---
+++ silvahnos-be/src/main/java/com/app/silvahnosbe/services/IngresoService.java
@@ -0,0 +1,28 @@
+package com.app.silvahnosbe.services;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.app.silvahnosbe.entities.IngresoEntity;
+import com.app.silvahnosbe.repositories.IngresoRepository;
+
+@Service
+public class IngresoService {
+    
+    @Autowired
+    IngresoRepository ingresoRepository;
+
+    public ArrayList<IngresoEntity> obtenerIngresos(){
+        return (ArrayList<IngresoEntity>) ingresoRepository.findAll();
+    }
+
+    public IngresoEntity obtenerIngresoPorId(Long id){
+        return ingresoRepository.findById(id).orElse(null);
+    }
+
+    public IngresoEntity guardarIngreso(IngresoEntity ingreso){
+        return ingresoRepository.save(ingreso);
+    }
+}
