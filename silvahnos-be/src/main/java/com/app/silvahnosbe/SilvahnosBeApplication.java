25176aaa8dab6e711b425ae832a54d382d174d38
LeoVergaraS <leo.vergara@usach.cl>
2023-04-17 18:20:59 -0400

    [Leo Vergara]-Inicializando el Backend

---
+++ silvahnos-be/src/main/java/com/app/silvahnosbe/SilvahnosBeApplication.java
@@ -0,0 +1,13 @@
+package com.app.silvahnosbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilvahnosBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilvahnosBeApplication.class, args);
+	}
+
+}
---
+++ silvahnos-be/src/test/java/com/app/silvahnosbe/SilvahnosBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.silvahnosbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilvahnosBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
