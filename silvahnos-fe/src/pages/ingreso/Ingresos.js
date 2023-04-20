cb91af3e46c720b33270cb50aa4554dc3dabb953
LeoVergaraS <leo.vergara@usach.cl>
2023-04-19 20:32:41 -0400

    [Leo Vergara]-Editar y eliminar ingreso

--- silvahnos-fe/src/pages/ingreso/Ingresos.js
+++ silvahnos-fe/src/pages/ingreso/Ingresos.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { Button, Col, Container, Modal, Row, Table } from 'react-bootstrap';
+import { Button, Col, Container, Form, Modal, Row, Table } from 'react-bootstrap';
 
 const Ingresos = () => {
     const [ingresos, setIngresos] = useState([]);
@@ -8,11 +8,88 @@ const Ingresos = () => {
     const [showDelete, setShowDelete] = useState(false);
     const [showEdit, setShowEdit] = useState(false);
 
-    const handleCloseDelete = () => setShowDelete(false);
-    const handleShowDelete = () => setShowDelete(true);
+    const handleCloseDelete = () =>{
+        setEditedItem(defaultItem);
+        setShowDelete(false);
+    }
 
-    const handleCloseEdit = () => setShowEdit(false);
-    const handleShowEdit = () => setShowEdit(true);
+    const handleShowDelete = (ingreso) =>{
+        setEditedItem(ingreso);
+        setShowDelete(true);
+    } 
+
+    const handleCloseEdit = () => {
+        setEditedItem(defaultItem);
+        setShowEdit(false)
+    };
+
+    const handleShowEdit = (ingreso) => {
+        setEditedItem(ingreso);
+        setShowEdit(true);
+    };
+
+    const handleChange = (e) => {
+        setEditedItem({
+            ...editedItem,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const updateIngreso = async () => {
+        try {
+            let url = 'http://localhost:8090/ingresos';
+            const response = await axios.post(url, editedItem);
+            if (response.status === 200) {
+                handleCloseEdit();
+                getIngresos();
+            }
+        } catch (err) {
+            console.log(err.message);
+        }
+    };
+
+    const handleDelete = () => {
+        editedItem.borrado = true;
+        deleteIngreso();
+    };
+
+    const deleteIngreso = async () => {
+        try {
+            let url = 'http://localhost:8090/ingresos';
+            const response = await axios.post(url, editedItem);
+            if (response.status === 200) {
+                handleCloseDelete();
+                getIngresos();
+            }
+        } catch (err) {
+            console.log(err.message);  
+        }
+    };
+
+
+    const [editedItem, setEditedItem] = useState({
+        id: null,
+        borrado: false,
+        fecha_creacion: null,
+        fecha_modificacion: null,
+        fecha_borrado: null,
+        patente: '',
+        monto: '',
+        descripcion: '',
+    });
+
+    const defaultItem = {
+        id: null,
+        borrado: false,
+        fecha_creacion: null,
+        fecha_modificacion: null,
+        fecha_borrado: null,
+        patente: '',
+        monto: '',
+        descripcion: '',
+    }
+
+
 
     const getIngresos = async () => {
         try {
@@ -24,6 +101,12 @@ const Ingresos = () => {
         } catch (err) {
             console.log(err.message);
         }
+    };
+
+    const formatearFecha = (fecha) => {
+        let fechaC = fecha.split('T')[0];
+        fechaC = fechaC.split('-');
+        return fechaC[2] + '/' + fechaC[1] + '/' + fechaC[0];
     };
 
     useEffect(() => {
@@ -53,13 +136,13 @@ const Ingresos = () => {
                             <tbody>
                                 {ingresos.map((ingreso) => (
                                     <tr key={ingreso.id}>
-                                        <td>{ingreso.fecha_creacion}</td>
+                                        <td>{formatearFecha(ingreso.fecha_creacion)}</td>
                                         <td>{ingreso.monto}</td>
                                         <td>{ingreso.patente}</td>
                                         <td>{ingreso.descripcion}</td>
                                         <td>
-                                            <Button variant='primary' onClick={handleShowEdit} style={{marginRight: 2}}>Editar</Button>
-                                            <Button variant='danger' onClick={handleShowDelete}>Eliminar</Button>
+                                            <Button variant='primary' onClick={() => handleShowEdit(ingreso)} style={{ marginRight: 2 }}>Editar</Button>
+                                            <Button variant='danger' onClick={() => handleShowDelete(ingreso)}>Eliminar</Button>
                                         </td>
                                     </tr>
                                 ))}
@@ -75,11 +158,24 @@ const Ingresos = () => {
                     <Modal.Title>Editar Ingreso</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <p>Editar ingreso</p>
+                    <Form>
+                        <Form.Group className='mb-3' controlId='formPatente'>
+                            <Form.Label>Patente</Form.Label>
+                            <Form.Control name="patente" type='text' value={editedItem.patente} onChange={handleChange}/>
+                        </Form.Group>
+                        <Form.Group className='mb-3' controlId='formMonto'>
+                            <Form.Label>Monto</Form.Label>
+                            <Form.Control name="monto" type='number' value={editedItem.monto} onChange={handleChange}/>
+                        </Form.Group>
+                        <Form.Group className='mb-3' controlId='formDescripcion'>
+                            <Form.Label>Descripción</Form.Label>
+                            <Form.Control name="descripcion" as='textarea' row={3} value={editedItem.descripcion} onChange={handleChange}/>
+                        </Form.Group>
+                    </Form>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant='secondary' onClick={handleCloseEdit}>Cerrar</Button>
-                    <Button variant='primary' onClick={handleCloseEdit}>Guardar</Button>
+                    <Button variant='primary' onClick={updateIngreso}>Guardar</Button>
                 </Modal.Footer>
             </Modal>
 
@@ -93,7 +189,7 @@ const Ingresos = () => {
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant='secondary' onClick={handleCloseDelete}>Cerrar</Button>
-                    <Button variant='danger' onClick={handleCloseDelete}>Eliminar</Button>
+                    <Button variant='danger' onClick={handleDelete}>Eliminar</Button>
                 </Modal.Footer>
             </Modal>
         </>
