17fc7c12919b78090f40293cbf4a6b56f0288165
Luis Toro <luis.toro.f@usach.cl>
2023-04-21 19:17:47 -0400

    [Luis Toro]-Update navbar responsive

--- silvahnos-fe/src/components/Navbar.js
+++ silvahnos-fe/src/components/Navbar.js
@@ -4,15 +4,9 @@ import Logo from "./images/logo.png";
 function BasicExample() {
 
   return (
-    <Navbar fixed="top" style={{ background: "#D9D9D9", color: "red" }}>
+    <Navbar collapseOnSelect expand="md" style={{ background: "#D9D9D9", color: "red"}}>
       <Container>
-        <Navbar.Brand href="#home" style={{ color: "white", fontWeight: "bold" }}>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-
-          <Nav className="ml-auto"  style={{ color: "black", fontWeight: "bold",}}>
-            <a href="/">
+        <Navbar.Brand href="#home" style={{ color: "white", fontWeight: "bold" }}>            <a href="/">
               {" "}
               <img
                 src={Logo}
@@ -20,23 +14,23 @@ function BasicExample() {
                 width="100"
                 height="50"
               />
-            </a>
-            <Nav.Link href="/ingresos">
-              Ingresos{" "}
-            </Nav.Link>
-            <Nav.Link href="/egresos">
-              Egresos{" "}
-            </Nav.Link>
-            <Nav.Link href="/facturas">
-              Facturas{" "}
-            </Nav.Link>
-            <Nav.Link href="/#logout" style={{position:"absolute", right:"80px"}}>
-              Cerrar sesión{" "}
-            </Nav.Link>
+            </a></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto" style={{ color: "black", fontWeight: "bold"}}>
+
+            <Nav.Link href="/ingresos">Ingresos{" "}</Nav.Link>
+            <Nav.Link href="/egresos">Egresos{" "}</Nav.Link>
+            <Nav.Link href="/facturas">Facturas{" "}</Nav.Link>
+          </Nav>
+          <Nav style={{ color: "black", fontWeight: "bold"}}>
+            <Nav.Link href="#logout">Cerrar sesión</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
+
+    
   );
 }
 
