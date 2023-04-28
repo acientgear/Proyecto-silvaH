43375adc41425eef7f61988fe0984df938ee6fe1
Luis Toro <luis.toro.f@usach.cl>
2023-04-27 21:51:00 -0400

    [Luis Toro]-Update vista home preliminar

--- silvahnos-fe/src/components/Navbar.js
+++ silvahnos-fe/src/components/Navbar.js
@@ -4,33 +4,34 @@ import Logo from "./images/logo.png";
 function BasicExample() {
 
   return (
-    <Navbar collapseOnSelect expand="md" style={{ background: "#D9D9D9", color: "red"}}>
+    <Navbar collapseOnSelect expand="md" style={{ background: "#D9D9D9", color: "red" }}>
       <Container>
-        <Navbar.Brand href="#home" style={{ color: "white", fontWeight: "bold" }}>            <a href="/">
-              {" "}
-              <img
-                src={Logo}
-                alt="Logo"
-                width="100"
-                height="50"
-              />
-            </a></Navbar.Brand>
+        <Navbar.Brand href="/" style={{ color: "white", fontWeight: "bold" }}>
+          <a href="/">
+            {" "}
+            <img
+              src={Logo}
+              alt="Logo"
+              width="100"
+              height="50"
+            />
+          </a></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto" style={{ color: "black", fontWeight: "bold"}}>
+          <Nav className="me-auto" style={{ color: "black", fontWeight: "bold" }}>
 
             <Nav.Link href="/ingresos">Ingresos{" "}</Nav.Link>
             <Nav.Link href="/egresos">Egresos{" "}</Nav.Link>
             <Nav.Link href="/facturas">Facturas{" "}</Nav.Link>
           </Nav>
-          <Nav style={{ color: "black", fontWeight: "bold"}}>
+          <Nav style={{ color: "black", fontWeight: "bold" }}>
             <Nav.Link href="#logout">Cerrar sesión</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
 
-    
+
   );
 }
 
--- silvahnos-fe/src/pages/Home.js
+++ silvahnos-fe/src/pages/Home.js
@@ -1,4 +1,4 @@
-import { Col, Row, Card, Button, Container, Table } from "react-bootstrap";
+import { Col, Row, Card, Button, Container, Table, Accordion, Badge, ListGroup, Tabs, Tab } from "react-bootstrap";
 import Ingresos from "../components/images/ingresos.png";
 import Egresos from "../components/images/egresos.png";
 import Facturas from "../components/images/facturas.png";
@@ -12,51 +12,12 @@ const Home = () => {
         </Col>
       </Row>
       <Row xs={1} sm={2} md={3}>
-        <Col>
-          
-            <Card >
-
-              <Card.Body>
-                <Card.Title>Últimos Ingresos</Card.Title>
-                <Card.Text>
-                  <Table striped responsive="sm" hover>
-                    <thead>
-                      <tr>
-                        <th style={{ width: '100px' }}>Fecha</th>
-                        <th style={{ width: '100px' }}>Patente</th>
-                        <th style={{ width: '100px' }}>Monto</th>
-                      </tr>
-
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>04-03-2021</td>
-                        <td>ASTARA</td>
-                        <td>$ 14.000</td>
-                      </tr>
-                      <tr>
-                        <td>04-03-2021</td>
-                        <td>TALLER</td>
-                        <td>$ 14.000</td>
-                      </tr>
-                      <tr>
-                        <td>04-03-2021</td>
-                        <td>TALLER</td>
-                        <td>$ 14.000</td>
-                      </tr>
-                    </tbody>
-                  </Table>
-                </Card.Text>
-                <Button variant="success" href="/crearIngreso">Registrar un ingreso</Button>
-              </Card.Body>
-            </Card>
-        </Col>
-        <Col>
-          <Card>
+        <Col >
+          <Card className="cardsH">
             <Card.Body>
-              <Card.Title>Últimos Egresos</Card.Title>
+              <Card.Title>Últimos Ingresos</Card.Title>
               <Card.Text>
-                <Table striped responsive="sm" hover>
+                <Table striped responsive="sm" hover bordered>
                   <thead>
                     <tr>
                       <th style={{ width: '100px' }}>Fecha</th>
@@ -83,22 +44,111 @@ const Home = () => {
                     </tr>
                   </tbody>
                 </Table>
+                <ListGroup>
+                  <ListGroup.Item>Total ingresos: $ 42.000</ListGroup.Item>
+                </ListGroup>
               </Card.Text>
-              <Button variant="danger" href="/crearEgreso">Registrar un egreso</Button>
+              <Button variant="success" href="/crearIngreso" style={{ backgroundColor: "#B8E7E1", color: "black", border: "none", fontWeight: "bold" }}>Registrar un ingreso</Button>
             </Card.Body>
           </Card>
         </Col>
         <Col>
-          <Card>
+          <Card className="cardsH">
             <Card.Body>
-              <Card.Title>Facturas por vencer</Card.Title>
+              <Card.Title>Egresos del mes de abril</Card.Title>
               <Card.Text>
+                <Accordion>
+                  <Accordion.Item eventKey="0">
+                    <Accordion.Header>Últimos registros</Accordion.Header>
+                    <Accordion.Body>
+                      <Table striped responsive="sm" hover>
+                        <thead>
+                          <tr>
+                            <th style={{ width: '100px' }}>Fecha</th>
+                            <th style={{ width: '100px' }}>Patente</th>
+                            <th style={{ width: '100px' }}>Monto</th>
+                          </tr>
 
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td>04-03-2021</td>
+                            <td>ASTARA</td>
+                            <td>$ 14.000</td>
+                          </tr>
+                          <tr>
+                            <td>04-03-2021</td>
+                            <td>TALLER</td>
+                            <td>$ 14.000</td>
+                          </tr>
+                          <tr>
+                            <td>04-03-2021</td>
+                            <td>TALLER</td>
+                            <td>$ 14.000</td>
+                          </tr>
+                        </tbody>
+                      </Table>
+                    </Accordion.Body>
+                  </Accordion.Item>
+                  <Accordion.Item eventKey="1">
+                    <Accordion.Header>Total egresos</Accordion.Header>
+                    <Accordion.Body>
+                      $ 42.000
+                    </Accordion.Body>
+                  </Accordion.Item>
+                </Accordion>
               </Card.Text>
-              <Button variant="primary" href="/crearFactura">Registrar una factura</Button>
+              <Button variant="danger" href="/crearEgreso" style={{ backgroundColor: "#F2B6A0", fontWeight: "bold", border: "none", color: "black" }}>Registrar un egreso</Button>
             </Card.Body>
           </Card>
-
+        </Col>
+        <Col>
+          <Card className="cardsH">
+            <Card.Body>
+              <Card.Title>Facturas más próximas a vencer</Card.Title>
+              <Card.Text>
+                <ListGroup as="ol" numbered>
+                  <ListGroup.Item
+                    as="li"
+                    className="d-flex justify-content-between align-items-start"
+                  >
+                    <div className="ms-2 me-auto">
+                      <div className="fw-bold">N° Factura: 131</div>
+                      Fecha de vencimiento: 04-03-2021
+                    </div>
+                    <Badge bg="primary" pill>
+                      Vence en 2 días
+                    </Badge>
+                  </ListGroup.Item>
+                  <ListGroup.Item
+                    as="li"
+                    className="d-flex justify-content-between align-items-start"
+                  >
+                    <div className="ms-2 me-auto">
+                      <div className="fw-bold">N° Factura: 132</div>
+                      Fecha de vencimiento: 04-04-2021
+                    </div>
+                    <Badge bg="primary" pill>
+                      Vence en 3 días
+                    </Badge>
+                  </ListGroup.Item>
+                  <ListGroup.Item
+                    as="li"
+                    className="d-flex justify-content-between align-items-start"
+                  >
+                    <div className="ms-2 me-auto">
+                      <div className="fw-bold">N° Factura: 133</div>
+                      Fecha de vencimiento: 04-05-2021
+                    </div>
+                    <Badge bg="primary" pill>
+                      Vence en 4 días
+                    </Badge>
+                  </ListGroup.Item>
+                </ListGroup>
+              </Card.Text>
+              <Button variant="primary" href="/crearFactura" style={{ backgroundColor: "#A5C0DD", border: "none", fontWeight: "bold", color: "black" }}>Registrar una factura</Button>
+            </Card.Body>
+          </Card>
         </Col>
       </Row>
     </Container >
