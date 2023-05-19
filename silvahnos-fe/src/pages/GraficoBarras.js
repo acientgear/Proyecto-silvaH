48dada164151a35be511342fdfbd8778a056a729
Luis Toro <luis.toro.f@usach.cl>
2023-05-19 17:42:27 -0400

    [Luis Toro]-Conexión gráfico de barras doble con BD

--- silvahnos-fe/src/pages/GraficoBarras.js
+++ silvahnos-fe/src/pages/GraficoBarras.js
@@ -1,37 +1,82 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Bar } from 'react-chartjs-2';
 import Sem1 from '../components/data/Sem1';
 import Sem2 from '../components/data/Sem2';
+import axios from 'axios';
 
-const data = {
-    labels: Sem1.concat(Sem2),
-    datasets: [
-        {
-            label: 'Ingresos',
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgba(255, 99, 132, 1)',
-            borderWidth: 1,
-            hoverBackgroundColor: 'rgba(255, 99, 132, 0.4)',
-            hoverBorderColor: 'rgba(255, 99, 132, 1)',
-            data: [65, 59, 80, 81, 56]
-        },
-        {
-            label: 'Egresos',
-            backgroundColor: 'rgba(54, 162, 235, 0.2)',
-            borderColor: 'rgba(54, 162, 235, 1)',
-            borderWidth: 1,
-            hoverBackgroundColor: 'rgba(54, 162, 235, 0.4)',
-            hoverBorderColor: 'rgba(54, 162, 235, 1)',
-            data: [28, 48, 40, 19, 86]
+const getTotalPorMes = async (tipo, anio, mes) => {
+    try {
+        let url = 'http://localhost:8090/' + tipo + '/total/' + anio + '/' + mes;
+        const response = await axios.get(url);
+        if (response.status === 200) {
+            return response.data;
         }
-    ]
-};
+    } catch (err) {
+        console.log(err.message);
+    }
+}
 
-const options = {
+async function getTotalMontosPorMes(tipo) {
+    const montos = [];
+    const fecha = new Date();
+    const anio = fecha.getFullYear();
+    const mes = fecha.getMonth();
 
-};
+    for (let i = 1; i <= 12; i++) {
+        const monto = getTotalPorMes(tipo, anio, i);
+        montos.push(monto);
+    }
+
+    const datos = await Promise.all(montos);
+
+    return datos;
+}
 
 const DoubleBarChart = () => {
+    const [montosIngresos, setMontosIngresos] = useState([]);
+    const [montosEgresos, setMontosEgresos] = useState([]);
+
+    useEffect(() => {
+        const fetchMontos = async () => {
+            const tipo1 = 'ingresos';
+            const tipo2 = 'egresos';
+
+            const montosIngresos = await getTotalMontosPorMes(tipo1);
+            const montosEgresos = await getTotalMontosPorMes(tipo2);
+
+            setMontosIngresos(montosIngresos);
+            setMontosEgresos(montosEgresos);
+        };
+
+        fetchMontos();
+    }, []);
+
+    const data = {
+        labels: Sem1.concat(Sem2),
+        datasets: [
+            {
+                label: 'Ingresos',
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)',
+                borderWidth: 1,
+                hoverBackgroundColor: 'rgba(255, 99, 132, 0.4)',
+                hoverBorderColor: 'rgba(255, 99, 132, 1)',
+                data: montosIngresos
+            },
+            {
+                label: 'Egresos',
+                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                borderColor: 'rgba(54, 162, 235, 1)',
+                borderWidth: 1,
+                hoverBackgroundColor: 'rgba(54, 162, 235, 0.4)',
+                hoverBorderColor: 'rgba(54, 162, 235, 1)',
+                data: montosEgresos
+            }
+        ]
+    };
+
+    const options = {};
+
     return (
         <div>
             <Bar
