3c210fbec949385498f317f70905f4b5602d3242
Luis Toro <luis.toro.f@usach.cl>
2023-05-23 02:36:16 -0400

    [Luis Toro]-Test obtener últimos ingresos

--- silvahnos-be/src/test/java/com/app/silvahnosbe/service/IngresoServiceTest.java
+++ silvahnos-be/src/test/java/com/app/silvahnosbe/service/IngresoServiceTest.java
@@ -10,6 +10,7 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -85,6 +86,72 @@ public class IngresoServiceTest {
         given(ingresoRepository.obtenerIngresos(2023,5)).willReturn(new ArrayList<>(List.of(ingreso, ingreso2)));
         //when
         List<IngresoEntity> ingresos=ingresoService.obtenerIngresos(2023,5);
+        // then
+        assertThat(ingresos).isNotNull();
+        assertThat(ingresos.size()).isEqualTo(2);
+    }
+
+    @DisplayName("test para obtener ingreso por id")
+    @Test
+    void testObtenerIngresoPorId(){
+        //given
+        given(ingresoRepository.findById(1l)).willReturn(Optional.of(ingreso));
+        //when
+        IngresoEntity ingreso1=ingresoService.obtenerIngresoPorId(1l);
+        //then
+        assertThat(ingreso1).isNotNull();
+    }
+
+    @DisplayName("test para obtenerlos últimos ingresos - caso ingresos > 3")
+    @Test
+    void testObtenerUltimosIngresos(){
+        //given
+        IngresoEntity ingreso2 = new IngresoEntity();
+        ingreso2.setDescripcion("Cambio pintura");
+        ingreso2.setId(2l);
+        ingreso2.setMonto(15000);
+        ingreso2.setOrigen("ASTARA");
+        ingreso2.setBorrado(false);
+        ingreso2.setFecha_creacion(new Timestamp(new Date().getTime()));
+
+        IngresoEntity ingreso3 = new IngresoEntity();
+        ingreso2.setDescripcion("Cambio pintura");
+        ingreso2.setId(3l);
+        ingreso2.setMonto(15000);
+        ingreso2.setOrigen("ASTARA");
+        ingreso2.setBorrado(false);
+        ingreso2.setFecha_creacion(new Timestamp(new Date().getTime()));
+
+        IngresoEntity ingreso4 = new IngresoEntity();
+        ingreso2.setDescripcion("Cambio pintura");
+        ingreso2.setId(4l);
+        ingreso2.setMonto(15000);
+        ingreso2.setOrigen("ASTARA");
+        ingreso2.setBorrado(false);
+        ingreso2.setFecha_creacion(new Timestamp(new Date().getTime()));
+        given(ingresoRepository.obtenerUltimosIngresos()).willReturn(new ArrayList<>(List.of(ingreso2,ingreso3,ingreso4)));
+        //when
+        List<IngresoEntity> ingresos=ingresoService.obtenerUltimosIngresos();
+        // then
+        assertThat(ingresos).isNotNull();
+        assertThat(ingresos.size()).isEqualTo(3);
+    }
+
+    @DisplayName("test para obtenerlos últimos ingresos - caso ingresos < 3")
+    @Test
+    void testObtenerUltimosIngresos2(){
+        //given
+        IngresoEntity ingreso2 = new IngresoEntity();
+        ingreso2.setDescripcion("Cambio pintura");
+        ingreso2.setId(2l);
+        ingreso2.setMonto(15000);
+        ingreso2.setOrigen("ASTARA");
+        ingreso2.setBorrado(false);
+        ingreso2.setFecha_creacion(new Timestamp(new Date().getTime()));
+
+        given(ingresoRepository.obtenerUltimosIngresos()).willReturn(new ArrayList<>(List.of(ingreso,ingreso2)));
+        //when
+        List<IngresoEntity> ingresos=ingresoService.obtenerUltimosIngresos();
         // then
         assertThat(ingresos).isNotNull();
         assertThat(ingresos.size()).isEqualTo(2);
