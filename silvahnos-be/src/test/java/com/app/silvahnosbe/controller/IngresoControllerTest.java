0d063d0b0cb211f4c1a6383cf3ac17f6a37a2cf5
Luis Toro <luis.toro.f@usach.cl>
2023-05-23 12:07:20 -0400

    [Luis Toro]-Test Controller - test para obtener todos los ingresos

--- silvahnos-be/src/test/java/com/app/silvahnosbe/controller/IngresoControllerTest.java
+++ silvahnos-be/src/test/java/com/app/silvahnosbe/controller/IngresoControllerTest.java
@@ -1,20 +1,62 @@
 package com.app.silvahnosbe.controller;
 
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
+import com.app.silvahnosbe.controllers.IngresoController;
+import com.app.silvahnosbe.entities.IngresoEntity;
 import com.app.silvahnosbe.services.IngresoService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
 
-public class IngresoControllerTest {
+import java.util.ArrayList;
+import java.util.List;
 
-    @Autowired
-    private MockMvc mockMvc;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
-    @MockBean
+@ExtendWith(MockitoExtension.class)
+class IngresoControllerTest {
+
+    @InjectMocks
+    private IngresoController ingresoController;
+
+    @Mock
     private IngresoService ingresoService;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+    @Test
+    void testGetAllIngresos_ExistenIngresos_ReturnsList() {
+        // Given
+        int anio = 2023;
+        int mes = 5;
+        List<IngresoEntity> ingresos = new ArrayList<>();
+        ingresos.add(new IngresoEntity());
+        when(ingresoService.obtenerIngresos(anio, mes)).thenReturn(ingresos);
+
+        // When
+        ResponseEntity<List<IngresoEntity>> response = ingresoController.getAllIngresos(anio, mes);
+
+        // Then
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(ingresos, response.getBody());
+    }
+
+    @Test
+    void testGetAllIngresos_NoExistenIngresos_ReturnsNotFound() {
+        // Given
+        int anio = 2023;
+        int mes = 5;
+        when(ingresoService.obtenerIngresos(anio, mes)).thenReturn(null);
+
+        // When
+        ResponseEntity<List<IngresoEntity>> response = ingresoController.getAllIngresos(anio, mes);
+
+        // Then
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
 }
