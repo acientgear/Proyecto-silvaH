6a63dee3cb81bbf52133e004908691f792b9c056
Luis Toro <luis.toro.f@usach.cl>
2023-06-15 09:25:07 -0400

    [Luis Toro]-LocalEntityTest

---
+++ silvahnos-be/src/test/java/com/app/silvahnosbe/entity/LocalEntityTest.java
@@ -0,0 +1,49 @@
+package com.app.silvahnosbe.entity;
+
+import org.junit.jupiter.api.Test;
+
+import com.app.silvahnosbe.entities.LocalEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LocalEntityTest {
+
+    @Test
+    void testId(){
+        // Given
+        LocalEntity local = new LocalEntity();
+
+        // When
+        local.setId(1l);
+        Long id = local.getId();
+
+        // Then
+        assertEquals(1, id);
+    }
+
+    @Test
+    void testNombre(){
+        // Given
+        LocalEntity local = new LocalEntity();
+
+        // When
+        local.setNombre("nombre");
+        String nombre = local.getNombre();
+
+        // Then
+        assertEquals("nombre", nombre);
+    }
+
+    @Test
+    void testDireccion(){
+        // Given
+        LocalEntity local = new LocalEntity();
+
+        // When
+        local.setDireccion("direccion");
+        String direccion = local.getDireccion();
+
+        // Then
+        assertEquals("direccion", direccion);
+    }    
+}
