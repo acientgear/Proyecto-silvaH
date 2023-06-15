f68b205249b9514d9ba070aa63c0ace01d2ce069
Luis Toro <luis.toro.f@usach.cl>
2023-06-15 09:45:10 -0400

    [Luis Toro]-EmailConfigTest

---
+++ silvahnos-be/src/test/java/com/app/silvahnosbe/entity/EmailConfigTest.java
@@ -0,0 +1,118 @@
+package com.app.silvahnosbe.entity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.app.silvahnosbe.entities.EmailConfig;
+
+public class EmailConfigTest {
+    @Test
+    void testId(){
+        // Given
+        EmailConfig emailConfig = new EmailConfig();
+
+        // When
+        emailConfig.setId(1l);
+        Long id = emailConfig.getId();
+
+        // Then
+        assertEquals(1, id);
+        
+    }
+
+    @Test
+    void testHost(){
+        // Given
+        EmailConfig emailConfig = new EmailConfig();
+
+        // When
+        emailConfig.setHost("host");
+        String host = emailConfig.getHost();
+
+        // Then
+        assertEquals("host", host);
+    }
+
+    @Test
+    void testPort(){
+        // Given
+        EmailConfig emailConfig = new EmailConfig();
+
+        // When
+        emailConfig.setPort(12345);
+        int port = emailConfig.getPort();
+
+        // Then
+        assertEquals(12345, port);
+    }
+
+    @Test
+
+    void testUsername(){
+        // Given
+        EmailConfig emailConfig = new EmailConfig();
+
+        // When
+        emailConfig.setUsername("username");
+        String username = emailConfig.getUsername();
+
+        // Then
+        assertEquals("username", username);
+    }
+
+    @Test
+    void testPassword(){
+        // Given
+        EmailConfig emailConfig = new EmailConfig();
+
+        // When
+        emailConfig.setPassword("password");
+        String password = emailConfig.getPassword();
+
+        // Then
+        assertEquals("password", password);
+    }
+
+    @Test
+    void testTexto(){
+        // Given
+        EmailConfig emailConfig = new EmailConfig();
+
+        // When
+        emailConfig.setTexto("texto");
+        String texto = emailConfig.getTexto();
+
+        // Then
+        assertEquals("texto", texto);
+    }
+
+    @Test
+    void testAuth(){
+        // Given
+        EmailConfig emailConfig = new EmailConfig();
+
+        // When
+        emailConfig.setAuth(true);
+        boolean auth = emailConfig.isAuth();
+
+        // Then
+        assertEquals(true, auth);
+    }
+
+    @Test
+    void testStarttls(){
+        // Given
+        EmailConfig emailConfig = new EmailConfig();
+
+        // When
+        emailConfig.setStarttls(true);
+        boolean starttls = emailConfig.isStarttls();
+
+        // Then
+        assertEquals(true, starttls);
+    }
+
+
+    
+}
