1945402aab2de195859041a038f07fda2f81a3b4
Luis Toro <luis.toro.f@usach.cl>
2023-06-19 02:35:22 -0400

    [Luis Toro]-IngresoServiceTest update

--- silvahnos-be/src/test/java/com/app/silvahnosbe/service/IngresoServiceTest.java
+++ silvahnos-be/src/test/java/com/app/silvahnosbe/service/IngresoServiceTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.verify;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -33,9 +34,10 @@ public class IngresoServiceTest {
     private IngresoService ingresoService;
 
     IngresoEntity ingreso;
+
     @BeforeEach
-    void setup(){
-        ingreso= new IngresoEntity();
+    void setup() {
+        ingreso = new IngresoEntity();
         ingreso.setId(1l);
         ingreso.setMonto(150000);
         ingreso.setDescripcion("Cuadratura BMW");
@@ -46,60 +48,60 @@ public class IngresoServiceTest {
 
     @DisplayName("test para guardar un ingreso")
     @Test
-    void testGuardarIngreso (){
+    void testGuardarIngreso() {
 
-        //given
+        // given
         given(ingresoRepository.save(ingreso)).willReturn(ingreso);
-        //when
-        IngresoEntity ingreso1=ingresoService.guardarIngreso(ingreso);
-        //then
+        // when
+        IngresoEntity ingreso1 = ingresoService.guardarIngreso(ingreso);
+        // then
         assertThat(ingreso1).isNotNull();
     }
 
     @DisplayName("test para listar ingresos null")
     @Test
-    void testListarIngresosNull(){
-        //given
+    void testListarIngresosNull() {
+        // given
         given(ingresoRepository.obtenerIngresos(2021, 8)).willReturn(null);
-        //when
+        // when
         ingresoService.obtenerIngresos(2021, 8);
-        //then
+        // then
         verify(ingresoRepository, times(1)).obtenerIngresos(2021, 8);
     }
 
     @DisplayName("test para listar ingresos")
     @Test
-    void testListarIngreso(){
-       //given
+    void testListarIngreso() {
+        // given
         IngresoEntity ingreso2 = new IngresoEntity();
         ingreso2.setDescripcion("Cambio pintura");
         ingreso2.setId(2l);
         ingreso2.setMonto(15000);
         ingreso2.setBorrado(false);
         ingreso2.setFecha_creacion(new Timestamp(new Date().getTime()));
-        //System.out.println(ingreso2.getFecha_creacion());
-        given(ingresoRepository.obtenerIngresos(2023,5)).willReturn(new ArrayList<>(List.of(ingreso, ingreso2)));
-        //when
-        List<IngresoEntity> ingresos=ingresoService.obtenerIngresos(2023,5);
+        // System.out.println(ingreso2.getFecha_creacion());
+        given(ingresoRepository.obtenerIngresos(2023, 5)).willReturn(new ArrayList<>(List.of(ingreso, ingreso2)));
+        // when
+        List<IngresoEntity> ingresos = ingresoService.obtenerIngresos(2023, 5);
         // then
         assertThat(ingresos.size()).isEqualTo(2);
     }
 
     @DisplayName("test para obtener ingreso por id")
     @Test
-    void testObtenerIngresoPorId(){
-        //given
+    void testObtenerIngresoPorId() {
+        // given
         given(ingresoRepository.findById(1l)).willReturn(Optional.of(ingreso));
-        //when
-        IngresoEntity ingreso1=ingresoService.obtenerIngresoPorId(1l);
-        //then
+        // when
+        IngresoEntity ingreso1 = ingresoService.obtenerIngresoPorId(1l);
+        // then
         assertThat(ingreso1.getId().equals(1l));
     }
 
     @DisplayName("test para obtenerlos últimos ingresos - caso ingresos > 3")
     @Test
-    void testObtenerUltimosIngresos(){
-        //given
+    void testObtenerUltimosIngresos() {
+        // given
         IngresoEntity ingreso2 = new IngresoEntity();
         ingreso2.setDescripcion("Cambio pintura");
         ingreso2.setId(2l);
@@ -120,17 +122,18 @@ public class IngresoServiceTest {
         ingreso2.setMonto(15000);
         ingreso2.setBorrado(false);
         ingreso2.setFecha_creacion(new Timestamp(new Date().getTime()));
-        given(ingresoRepository.obtenerUltimosIngresos()).willReturn(new ArrayList<>(List.of(ingreso2,ingreso3,ingreso4)));
-        //when
-        List<IngresoEntity> ingresos=ingresoService.obtenerUltimosIngresos();
+        given(ingresoRepository.obtenerUltimosIngresos())
+                .willReturn(new ArrayList<>(List.of(ingreso2, ingreso3, ingreso4)));
+        // when
+        List<IngresoEntity> ingresos = ingresoService.obtenerUltimosIngresos();
         // then
         assertThat(ingresos.size()).isEqualTo(3);
     }
 
     @DisplayName("test para obtenerlos últimos ingresos - caso ingresos < 3")
     @Test
-    void testObtenerUltimosIngresos2(){
-        //given
+    void testObtenerUltimosIngresos2() {
+        // given
         IngresoEntity ingreso2 = new IngresoEntity();
         ingreso2.setDescripcion("Cambio pintura");
         ingreso2.setId(2l);
@@ -138,9 +141,9 @@ public class IngresoServiceTest {
         ingreso2.setBorrado(false);
         ingreso2.setFecha_creacion(new Timestamp(new Date().getTime()));
 
-        given(ingresoRepository.obtenerUltimosIngresos()).willReturn(new ArrayList<>(List.of(ingreso,ingreso2)));
-        //when
-        List<IngresoEntity> ingresos=ingresoService.obtenerUltimosIngresos();
+        given(ingresoRepository.obtenerUltimosIngresos()).willReturn(new ArrayList<>(List.of(ingreso, ingreso2)));
+        // when
+        List<IngresoEntity> ingresos = ingresoService.obtenerUltimosIngresos();
         // then
         assertThat(ingresos.size()).isEqualTo(2);
     }
@@ -148,7 +151,7 @@ public class IngresoServiceTest {
     @DisplayName("test para obtener el total de los ingresos")
     @Test
     void testObtenerMontoPorMes() {
-        //given
+        // given
         IngresoEntity ingreso2 = new IngresoEntity();
         ingreso2.setDescripcion("pintura");
         ingreso2.setId(2l);
@@ -158,11 +161,11 @@ public class IngresoServiceTest {
         ingreso3.setDescripcion("pintura");
         ingreso3.setId(2l);
         ingreso3.setMonto(15000);
-        given(ingresoRepository.obtenerIngresos(2023,3)).willReturn(List.of(ingreso,ingreso2,ingreso3));
+        given(ingresoRepository.obtenerIngresos(2023, 3)).willReturn(List.of(ingreso, ingreso2, ingreso3));
 
-        //when
-        int Ingresos=ingresoService.obtenerTotalIngresosPorMes(2023,3);
-        //then
+        // when
+        int Ingresos = ingresoService.obtenerTotalIngresosPorMes(2023, 3);
+        // then
         assertThat(Ingresos).isEqualTo(180000);
 
     }
@@ -170,26 +173,36 @@ public class IngresoServiceTest {
     @DisplayName("test para obtener el total de los ingresos por dia")
     @Test
     void testObtenerMontoPorDia() {
-        //given
-        given(ingresoRepository.obtenerMontoPorDia(2023,5,23)).willReturn(180000);
-        //when
-        int Ingresos=ingresoService.obtenerMontoPorDia(2023,5,23);
-        //then
+        // given
+        given(ingresoRepository.obtenerMontoPorDia(2023, 5, 23)).willReturn(180000);
+        // when
+        int Ingresos = ingresoService.obtenerMontoPorDia(2023, 5, 23);
+        // then
         assertThat(Ingresos).isEqualTo(180000);
 
+    }
+
+    @DisplayName("test para obtener el total de los ingresos por mes cuando es nulo o 0")
+    @Test
+    void testObtenerMontoPorMesNull() {
+        // given
+        given(ingresoRepository.obtenerIngresos(2023, 5)).willReturn(Collections.emptyList());
+        // when
+        int ingresos = ingresoService.obtenerTotalIngresosPorMes(2023, 5);
+        // then
+        assertThat(ingresos).isEqualTo(0);
     }
 
     @DisplayName("test para obtener el saldo de la cuenta")
     @Test
     void testObtenerSaldoCuenta() {
-        //given
+        // given
         given(ingresoRepository.obtenerSaldoCuenta(5)).willReturn(180000);
-        //when
-        int Ingresos=ingresoService.obtenerSaldoCuenta(5);
-        //then
+        // when
+        int Ingresos = ingresoService.obtenerSaldoCuenta(5);
+        // then
         assertThat(Ingresos).isEqualTo(180000);
 
     }
 
-    
 }
