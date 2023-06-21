81114faf2266497a48b43a87359b3dfdf56bb78c
IgnacioGrez <ignacio.grez.m@usach.cl>
2023-06-20 22:30:27 -0400

    [Ignacio Grez] Fechas arregladas.

--- silvahnos-be/src/main/java/com/app/silvahnosbe/SilvahnosBeApplication.java
+++ silvahnos-be/src/main/java/com/app/silvahnosbe/SilvahnosBeApplication.java
@@ -3,12 +3,14 @@ package com.app.silvahnosbe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
 public class SilvahnosBeApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(SilvahnosBeApplication.class, args);
 	}
 }
