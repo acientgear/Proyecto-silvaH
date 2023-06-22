98f9de27c92059ac2576eeaa7e0dcb98ccb11c64
LeoVergaraS <leo.vergara@usach.cl>
2023-06-21 22:26:12 -0400

    [Leo Vergara]-Seccion hover

--- silvahnos-fe/src/pages/admin/AdminView.js
+++ silvahnos-fe/src/pages/admin/AdminView.js
@@ -21,9 +21,17 @@ const AdminView = () => {
         setSeccion(seccion);
     }
 
+    const handleActive = (sec) => {
+        if (sec === seccion) {
+            return "true";
+        } else {
+            return "false";
+        }
+    }
+
     const getMotivos = () => {
         setMotivosHTML(
-            <div style={{marginTop: "10px"}}>
+            <div style={{ marginTop: "10px" }}>
                 <h1>Motivos</h1>
                 <Row xs={1} lg={2}>
                     <Col style={{ display: "flex", justifyContent: "center", alignItems: "start" }}>
@@ -39,7 +47,7 @@ const AdminView = () => {
 
     const getFacturas = () => {
         setFacturasHTML(
-            <div style={{marginTop: "10px"}}>
+            <div style={{ marginTop: "10px" }}>
                 <h1>Facturas</h1>
                 <Row >
                     <Col style={{ display: "flex", justifyContent: "center", alignItems: "start" }}>
@@ -52,7 +60,7 @@ const AdminView = () => {
 
     const getCorreos = () => {
         setCorreosHTML(
-            <div style={{marginTop: "10px"}}>
+            <div style={{ marginTop: "10px" }}>
                 <h1>Correos</h1>
                 <Row xs={1} lg={2}>
                     <Col style={{ display: "flex", justifyContent: "center", alignItems: "start" }}>
@@ -71,28 +79,44 @@ const AdminView = () => {
             <aside className='sidebar'>
                 <div className='nav'>
                     <ul className='nav-collapse'>
-                        <li className='link-collapse'><a className="link-anchor-collapse" href='#usuarios' onClick={() => handleSeccion("usuarios")}><AiOutlineUser /></a></li>
-                        <li className='link-collapse'><a className="link-anchor-collapse" href='#motivos' onClick={() => handleSeccion("motivos")}><AiOutlineFall /></a></li>
-                        <li className='link-collapse'><a className="link-anchor-collapse" href='#facturas' onClick={() => handleSeccion("facturas")}><AiOutlineFileText /></a></li>
-                        <li className='link-collapse'><a className="link-anchor-collapse" href='#correos' onClick={() => handleSeccion("correos")}><AiOutlineMail /></a></li>
+                        <li active={handleActive("usuarios")} className='link-collapse'>
+                            <a className="link-anchor-collapse" href='#usuarios' onClick={() => handleSeccion("usuarios")}>
+                                <AiOutlineUser />
+                            </a>
+                        </li>
+                        <li active={handleActive("motivos")} className='link-collapse'>
+                            <a className="link-anchor-collapse" href='#motivos' onClick={() => handleSeccion("motivos")}>
+                                <AiOutlineFall />
+                            </a>
+                        </li>
+                        <li active={handleActive("facturas")} className='link-collapse'>
+                            <a className="link-anchor-collapse" href='#facturas' onClick={() => handleSeccion("facturas")}>
+                                <AiOutlineFileText />
+                            </a>
+                        </li>
+                        <li active={handleActive("correos")} className='link-collapse'>
+                            <a className="link-anchor-collapse" href='#correos' onClick={() => handleSeccion("correos")}>
+                                <AiOutlineMail />
+                            </a>
+                        </li>
                     </ul>
                     <ul className='nav-always'>
-                        <li className='link-always'>
+                        <li active={handleActive("usuarios")} className='link-always'>
                             <a className='link-anchor-always' href='#usuarios' onClick={() => handleSeccion("usuarios")}>
                                 <AiOutlineUser /> Usuario
                             </a>
                         </li>
-                        <li className='link-always'>
+                        <li active={handleActive("motivos")} className='link-always'>
                             <a className='link-anchor-always' href='#motivos' onClick={() => handleSeccion("motivos")}>
                                 <AiOutlineFall /> Motivos
                             </a>
                         </li>
-                        <li className='link-always'>
+                        <li active={handleActive("facturas")} className='link-always'>
                             <a className='link-anchor-always' href='#facturas' onClick={() => handleSeccion("facturas")}>
                                 <AiOutlineFileText /> Factura
                             </a>
                         </li>
-                        <li className='link-always'>
+                        <li active={handleActive("correos")} className='link-always'>
                             <a className='link-anchor-always' href='#correos' onClick={() => handleSeccion("correos")}>
                                 <AiOutlineMail /> Correo
                             </a>
