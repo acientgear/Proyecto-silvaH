7c438257582e357cb93093a0090369b44706e5a7
Luis Toro <luis.toro.f@usach.cl>
2023-07-19 02:04:46 -0400

    [Luis Toro]-TokenInforTest

---
+++ silvahnos-be/src/test/java/com/app/silvahnosbe/models/TokenInfoTest.java
@@ -0,0 +1,37 @@
+package com.app.silvahnosbe.models;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class TokenInfoTest {
+    
+    @Test
+public void testTokenInfoConstructor_SetsJwtToken() {
+    // Given
+    String jwtToken = "testToken";
+
+    // When
+    TokenInfo tokenInfo = new TokenInfo(jwtToken);
+
+    // Then
+    assertEquals(jwtToken, tokenInfo.getJwtToken());
+}
+
+@Test
+public void testTokenInfoGetJwtToken_ReturnsJwtToken() {
+    // Given
+    String jwtToken = "testToken";
+    TokenInfo tokenInfo = new TokenInfo(jwtToken);
+
+    // When
+    String result = tokenInfo.getJwtToken();
+
+    // Then
+    assertEquals(jwtToken, result);
+}
+
+}
