427d16a24c81115cf8833c91000cb60981fadbec
Luis Toro <luis.toro.f@usach.cl>
2023-07-19 02:10:33 -0400

    [Luis Toro]-AppTest

--- silvahnos-be/src/test/java/com/app/silvahnosbe/SilvahnosBeApplicationTests.java
+++ silvahnos-be/src/test/java/com/app/silvahnosbe/SilvahnosBeApplicationTests.java
@@ -1,12 +1,16 @@
 package com.app.silvahnosbe;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SilvahnosBeApplicationTests {
+@ExtendWith(MockitoExtension.class)
+public class SilvahnosBeApplicationTests {
 
-	//@Test
-	//void contextLoads() {
-	//}
-
+    @Test
+    public void contextLoads() {
+        // Verificar que el contexto de la aplicación se cargue sin errores
+    }
 }
+
