f9f6f0028234bbac7a4fabad96a436a5bee6d993
IgnacioGrez <ignacio.grez.m@usach.cl>
2023-07-22 01:16:44 -0400

    [Ignacio Grez] Backup BD remoto gDrive

--- backup.sh
+++ backup.sh
@@ -2,9 +2,16 @@
 
 # Backup file path
 BACKUP_PATH="/root/backups"
-BACKUP_NAME="backup3.sql"
 
 BACKUP_FILE_NAME="$(date +"backup-%d-%m-%y-%H%M%S.sql")"
 
+BACKUP_CLOUD_PATH="backups"
+
+RCLONE_CLOUD_REMOTE="backups"
+
 # Run mysqldmp command
 docker exec mysql-db bash -c 'exec mysqldump -u root -p"$MYSQL_ROOT_PASSWORD" --databases "$MYSQL_DATABASE"'> $BACKUP_PATH/$BACKUP_FILE_NAME ;
+
+rclone -v copy $BACKUP_PATH $RCLONE_CLOUD_REMOTE:$BACKUP_CLOUD_PATH; 
+
+rm $BACKUP_PATH/$BACKUP_FILE_NAME;
