1e4faf2d9dc6ee6025cb983592c50bdbd899c924
Luis Toro <luis.toro.f@usach.cl>
2023-07-25 11:56:56 -0400

    [Luis Toro]-Documentación services faltantes

--- silvahnos-be/src/main/java/com/app/silvahnosbe/services/MontoService.java
+++ silvahnos-be/src/main/java/com/app/silvahnosbe/services/MontoService.java
@@ -18,10 +18,22 @@ public class MontoService {
     @Autowired
     MontoRepository montoRepository;
 
+    /**
+    *funcion que obtiene los montos de ingreso totales por cada mes de un año determinado
+    * @param int anio
+    * @return retorn una lista con los montos de ingreso totales por cada mes de un año determinado
+    */
+
     public List<Monto> obtenerMontoIngresoTotalMesAnual(int anio){
         List<Monto> montos = (List<Monto>) montoRepository.obtenerMontoIngresoTotalMesAnual(anio);
         return montos;
     }
+
+    /**
+    *funcion que obtiene los montos de egreso totales por cada mes de un año determinado
+    * @param int anio
+    * @return retorn una lista con los montos de egreso totales por cada mes de un año determinado
+    */
 
     public List<Monto> obtenerMontoEgresoTotalMesAnual(int anio){
         return (List<Monto>) montoRepository.obtenerMontoEgresoTotalMesAnual(anio);
